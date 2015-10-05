7de6319797cfdd6b1a7a03d8e7b98f681edbea28
Aijun Bai <aijunbai@gmail.com>
2015-10-04 21:58:29 -0700

    added data/human_track.rcg demo file

--- qt4/moc_config_dialog.cpp
+++ qt4/moc_config_dialog.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'config_dialog.h'
 **
-** Created: Sun Oct 4 21:48:03 2015
+** Created: Sun Oct 4 21:58:05 2015
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.1)
 **
 ** WARNING! All changes made in this file will be lost!
